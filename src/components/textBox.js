4df529f1b67d67b048cdadcef92c72520b58fc2b
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-07-04 12:44:06 +0100

    Update textBox.js

--- src/components/textBox.js
+++ src/components/textBox.js
@@ -1,21 +1,23 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Box
-  } from '@chakra-ui/react'
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Box,
+  Text,
+  Image,
+} from '@chakra-ui/react'
 
-  import { useDisclosure } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
 
-  import { useState, useEffect, useRef } from "react";
-
-  import React from 'react';
+import { useDisclosure } from '@chakra-ui/react'
+import { useState, useEffect, useRef } from "react";
+import React from 'react';
 
 export default function DrawerExample() {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -27,6 +29,7 @@ export default function DrawerExample() {
           Open
         </Button>
         <Drawer
+          size={'full'}
           isOpen={isOpen}
           placement='right'
           onClose={onClose}
@@ -38,6 +41,27 @@ export default function DrawerExample() {
             <DrawerHeader>Create your account</DrawerHeader>
   
             <DrawerBody>
+              <Box >
+                <Text pb={100}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec eget ex euismod, egestas nunc eu, consectetur
+                  nisi.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec eget ex euismod, egestas nunc eu, consectetur
+                  nisi.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec eget ex euismod, egestas nunc eu, consectetur
+                  nisi.
+                </Text>
+
+                <Flex>
+                <Spacer />
+                  <Box aling>
+                    <Image src='https://via.placeholder.com/500' />
+                  </Box>
+                </Flex>
+              </Box>
+
             </DrawerBody>
   
             <DrawerFooter>
