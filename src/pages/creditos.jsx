003102e03891deaaee8942317fe710b267b5a856
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-07-06 20:23:13 +0100

    Update creditos.jsx

--- src/pages/creditos.jsx
+++ src/pages/creditos.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Link from 'next/link'
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Link from "next/link";
 
 //Default theme
-import { ColorModeScript } from '@chakra-ui/react'
-import theme from '../components/theme'
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "../components/theme";
 
 const creditos = () => {
   return (
     <div className={styles.container}>
-    <Head>
-      <title>Creditos | SLQInfo</title>
-      <meta name="description" content="SQL | Everything that you should know " />
-      <link rel="icon" href="/mysql.png" />
-    </Head>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <Navbar />
+      <Head>
+        <title>Creditos | SLQInfo</title>
+        <meta
+          name="description"
+          content="SQL | Everything that you should know "
+        />
+        <link rel="icon" href="/mysql.png" />
+      </Head>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Navbar />
 
-    <div className=''>
-
+      <div className=""></div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default creditos
+export default creditos;
