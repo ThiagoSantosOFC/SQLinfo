07ba4e3406c6ecadccb32a04c94f7b65e9cfc604
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-07-06 21:06:52 +0100

    Update creditos.jsx

--- src/pages/creditos.jsx
+++ src/pages/creditos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
-import Link from "next/link";
+
 
 //Default theme
 import { ColorModeScript } from "@chakra-ui/react";
@@ -32,7 +32,7 @@ const creditos = () => {
             Este site foi criado para propositos escolares.
             <br />
             Correspondentes ao módulo 14 da disciplina de Programação de
-            sistemas informaticos.
+            sistemas informáticos.
             <br />
             Feito por Thiago Santos nº20 e Pedro Kaleb nº17.
             Professor Marco Neves.
