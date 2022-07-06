0973544c8b71854b2877b4487fb98986cbd93cf7
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-07-06 09:30:40 +0100

    pages/index:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 
@@ -9,7 +8,10 @@ import { ColorModeScript } from '@chakra-ui/react'
 import theme from '../components/theme'
 
 //Components chakra
-import { Heading, Box } from '@chakra-ui/react'
+import { Heading, Text} from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
 
 //Self components
 import ContentDawer from '../components/contentDawer'
@@ -25,13 +27,45 @@ export default function Home() {
       </Head>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Navbar />
-      <Box>
-        <ContentDawer
-          title="SQL"
-          button_text = "SQL"
-        >  
-          Loren 
-        </ContentDawer>        
+
+      {/* Website header content */}
+      <Box p={50} pb={400} rounded={'full'} bg={'white'}>
+        <Stack direction={'column'} align={"center"} spacing={10}>
+          <Box>
+            <Image src="/images/sql/sql-header.png" alt="logo" />
+          </Box>
+        </Stack>
+      </Box>
+
+
+      {/* Sql content */}
+      <Box pt={400} p={50}>
+          <Box>
+            <Stack align={'start'} direction={'row'}>
+              <Box>
+                <Heading as="h1" size="xl" color={'white'}>
+                  SQL
+                </Heading>
+              </Box>
+            </Stack>
+            <Stack direction={'row'} pt={10} spacing={40}>
+              <Box>
+                <Text color={'white'} maxWidth={700}>
+                  Structured Query Language, ou Linguagem de Consulta Estruturada ou SQL, é a linguagem de 
+                  pesquisa declarativa padrão para banco de dados relacional. 
+                  Muitas das características originais do SQL foram inspiradas na álgebra relacional. 
+                    <br /> <br />
+                  A Linguagem SQL surgiu no início dos anos 70, como fruto de um projeto da IBM no laboratório
+                  de San José. Eles trabalhavam num projeto de uma linguagem que se adequasse ao modelo relacional.
+                  Este projeto trabalhava em paralelo com um outro projeto que visava desenvolver um 
+                  sistema gerência de Banco de Dados relacional, chamado "System R".
+                </Text>
+              </Box>
+              <Box>
+                <Image src="/images/sql/image-sql.png" alt="logo"/>
+              </Box>
+            </Stack>
+          </Box>       
       </Box>
     </div>
   )
