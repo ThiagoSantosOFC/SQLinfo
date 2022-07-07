347b73d8aee68e94133b79c1417a4c886fba90e5
Thiago Santos <t.mateus.santos2004@gmail.com>
2022-07-07 12:48:39 +0100

    Update unioes.jsx

--- src/components/unioes.jsx
+++ src/components/unioes.jsx
@@ -1,46 +1,61 @@
-import React from 'react'
+import React from "react";
 //Components chakra
 import { Heading, Text } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import { Stack } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 
-
 const unioes = () => {
   return (
     <div>
-    {/* Unioes */}
-    <Box pt={400} p={50} pb={400} id="unioes">
-      <Box>
-        <Stack align={"start"}>
-          <Box>
-            <Heading as="h1" size="xl" color={"white"}>
-              Uniões
-            </Heading>
-          </Box>
-        </Stack>
-        <Stack direction={"row"} pt={10} spacing={40}>
-          <Box>
-            <Image src="/images/sql/image-sql.png" alt="logo" />
-          </Box>
-          <Box>
-            <Text color={"white"} maxWidth={700}>
-              Structured Query Language, ou Linguagem de Consulta Estruturada
-              ou SQL, é a linguagem de pesquisa declarativa padrão para banco
-              de dados relacional. Muitas das características originais do SQL
-              foram inspiradas na álgebra relacional.
-              <br /> <br />A Linguagem SQL surgiu no início dos anos 70, como
-              fruto de um projeto da IBM no laboratório de San José. Eles
-              trabalhavam num projeto de uma linguagem que se adequasse ao
-              modelo relacional. Este projeto trabalhava em paralelo com um
-              outro projeto que visava desenvolver um sistema gerência de
-              Banco de Dados relacional, chamado "System R".
-            </Text>
-          </Box>
-        </Stack>
+      {/* Unioes */}
+      <Box pt={400} p={50} pb={400} id="unioes">
+        <Box>
+          <Stack align={"start"}>
+            <Box>
+              <Heading as="h1" size="xl" color={"white"}>
+                Uniões
+              </Heading>
+            </Box>
+          </Stack>
+          <Stack direction={"row"} pt={10} spacing={40}>
+            <Box>
+              <Image src="/images/sql/image-sql.png" alt="logo" />
+            </Box>
+            <Box>
+              <Text color={"white"} maxWidth={700}>
+                <strong>UNION</strong> é usado para combinar dois ou mais comandos separados para
+                retornar uma única saída.
+                <br />
+                O operador <strong>UNION</strong> é usado para combinar o conjunto de
+                resultados de dois ou mais comandos <strong>SELECT</strong>. 
+                <br />
+                <strong>UNION</strong> é útil porque
+                temos algumas informações em várias tabelas e queremos pegar
+                linhas de todas elas de uma só vez.
+                <br />
+                Queremos selecionar vários
+                conjuntos de linhas de uma mesma tabela, mas algumas condições
+                que caracterizam cada conjunto não são fáceis de escrever como
+                uma única cláusula WHERE. 
+                <br />
+                UNION permite a recuperação de cada
+                conjunto com uma cláusula WHERE mais simples em sua própria
+                declaração SELECT. 
+                <br />
+                As linhas recuperadas por cada uma são
+                combinadas e produzidas como resultado final do comando.
+                <br /> 
+                As uniões são úteis se você desejar juntar os resultados de mais de
+                uma consulta, mas esteja ciente de que o número das colunas
+                retornadas por cada consulta selecionada deve ser o mesmo.
+              </Text>
+            </Box>
+          </Stack>
+        </Box>
       </Box>
-    </Box></div>
-  )
-}
+    </div>
+  );
+};
 
-export default unioes
+export default unioes;
