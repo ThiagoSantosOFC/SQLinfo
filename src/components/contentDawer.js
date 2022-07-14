ae4498032b74d813379c95cfe1986af0d96d2237
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-07-14 13:01:17 +0100

    Update contentDawer.js

--- src/components/contentDawer.js
+++ src/components/contentDawer.js
@@ -29,9 +29,12 @@ export default function ContentDawer(props) {
     "Loren ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   return (
-    <Box>
+    <Box 
+    paddingTop={ props.paddingTop == undefined ? "auto" : props.paddingTop }
+    PaddingBottom={ props.PaddingBottom == undefined ? "auto" : props.scrollPaddingBottom }
+    >
       <Tip label={props.tip}>
-        <Button ref={btnRef} onClick={onOpen}>
+        <Button ref={btnRef} onClick={onOpen} mr={3} shadow={'dark-lg'} >
           {props.button_text == undefined ? "Button text" : props.button_text}
         </Button>
       </Tip>
